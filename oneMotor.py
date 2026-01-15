2b73cc075af5c098993e8f8713e6434dacd566ae
Olivia Allen <omallen@asu.edu>
2026-01-14 19:50:16 -0700

    Update oneMotor.py
    i made everything :) unless it didnt work then jaiden did

--- oneMotor.py
+++ oneMotor.py
@@ -1,7 +1,25 @@
+# Raspberry Pi: control motors using L293D
+from gpiozero import Motor, OutputDevice
 from time import sleep
-from gpiozero import PWMOutputDevice, DigitalOutputDevice
-
-motorSpeedForward = 0
-motorSpeedBackward = 0
 
 
+# Make pins to output signals
+motor1 = Motor(16, 20, enable=21)
+motor2 = Motor(19, 26, enable=13)
+
+print("Running the motors...")
+
+while True:
+    # Rotate motors in different directions:
+    motor1.forward()
+    motor2.forward()
+    sleep(3)
+    motor1.stop()
+    motor2.stop()
+    sleep(0.1)
+    motor1.backward()
+    motor2.backward()
+    sleep(3)
+    motor1.stop()
+    motor2.stop()
+    sleep(3)
